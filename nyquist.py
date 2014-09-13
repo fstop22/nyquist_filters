79eaba6c001c4055659178e5b05f5a79facdbc4d
fstop22 <jeff.grasty@gmail.com>
2014-09-13 11:20:30 +0100

    Cleaned up typos

--- nyquist.py
+++ nyquist.py
@@ -12,6 +12,58 @@ def _FftSize(Q):
     return int(sp.floor(4096.0/Q)*Q)
 
 def _design(N, Q, alpha, desired, mu=1.0e-2, eps=1.0e12):
+    
+    '''
+    Generalized algorithm for designing Nyquist or root Nyquist
+    shape filters
+
+    Uses remez algorithm to initially design an almost nyquist filter
+    with Q samples per symbol, lenght N, and excess bandwith of alpha.
+    The adjusts pass band cutoff frequency via gradient descent algorithm
+    until |H(0.5)| = desired.  For nyquist filter |H(0.5)| = 0.5, 
+    for root Nyquist filter |H(0.5)| = sqrt(2)/2. 
+
+     |H(f)|   ^
+              |
+             1|----------
+              |         | \
+         ^    |         |  \
+         |    |         |   \
+      desired |---------|---|\
+         |    |         |   | \
+         v    |         |   |  \
+              |         |   |   \
+              +---------|---|---|--------|---> f
+              0        1-a  1  1+a      Q/2
+                       ---  -  ---
+                        2   2   2
+                      <--->
+                Move passband cutoff freq until |H(0.5)| = desired
+    
+    Called by Nyquist and rootNyquist functions to design filters,
+    respectively.
+    
+    Parameters
+    ----------
+    N : int
+        Number of taps of filter
+    Q : int
+        Samples per symbol
+    alpha : float
+        Excess bandwidth factor (0 < alpha <= 1.0)
+    desired : float
+        Desired magnitude of filter at inflection point f = 0.5.  For nyquist filter
+        set to 0.5.  For root-nyquist filter set to sqrt(2)/2
+    mu : float
+        Gradient descent adaptation rate
+    eps : float
+        Error tolerance
+
+    Returns
+    -------
+    h : ndarray
+        Filter coefficients
+    '''
 
     # Initial pass and stop bands of filter
     Fs = float(Q)
